1bbbea8c7bdcf7b6aa2bbc4c5814fc5821291ad8
Lukas <lukas.ikvm1@gmail.com>
2022-05-09 19:16:14 +0200

    Update spring data elasticsearch reactive configuration

---
+++ src/main/java/com/example/searchservice/configuration/Config.java
@@ -0,0 +1,32 @@
+package com.example.searchservice.configuration;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.reactive.ReactiveElasticsearchClient;
+import org.springframework.data.elasticsearch.client.reactive.ReactiveRestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.config.AbstractReactiveElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
+
+@Configuration
+@EnableReactiveElasticsearchRepositories(basePackages = "com.example.searchservice.repository")
+@ComponentScan(basePackages = {"com.example.searchservice"})
+public class Config extends AbstractReactiveElasticsearchConfiguration {
+
+    @Value("${elasticsearch.url}")
+    public String elasticUrl;
+
+
+    @Override
+    @Bean
+    public ReactiveElasticsearchClient reactiveElasticsearchClient() {
+        final ClientConfiguration configuration = ClientConfiguration.builder()
+                .connectedTo(elasticUrl)
+                .build();
+        return ReactiveRestClients.create(configuration);
+    }
+}
