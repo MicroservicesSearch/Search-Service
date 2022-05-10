2d0470d5cc4a6495d2874a41ae8e1c0860f193e3
Lukas <lukas.ikvm1@gmail.com>
2022-05-10 11:16:48 +0200

    reformat code

--- src/main/java/com/example/searchservice/SearchServiceApplication.java
+++ src/main/java/com/example/searchservice/SearchServiceApplication.java
@@ -2,10 +2,7 @@ package com.example.searchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.elasticsearch.repository.config.EnableReactiveElasticsearchRepositories;
+
 
 @SpringBootApplication
 public class SearchServiceApplication {
