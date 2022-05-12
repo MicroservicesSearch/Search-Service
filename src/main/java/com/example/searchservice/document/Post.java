df6f1e947507f10f9a3913072884cb04593ef5a1
Lukas <lukas.ikvm1@gmail.com>
2022-05-12 17:42:13 +0200

    Add post entity

---
+++ src/main/java/com/example/searchservice/document/Post.java
@@ -0,0 +1,48 @@
+package com.example.searchservice.document;
+
+
+import com.example.searchservice.document.helper.Indices;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Document(indexName = Indices.POST_INDEX)
+@Setting(settingPath = "static/es-settings.json")
+@JsonIgnoreProperties(value = { "_class" })
+public class Post {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private Long id;
+    @Field(type = FieldType.Keyword)
+    private String userId;
+    @Field(type = FieldType.Text)
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
