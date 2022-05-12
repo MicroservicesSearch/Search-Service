16360a648e14ce730c221db6e6c4a653bace5f5f
Lukas <lukas.ikvm1@gmail.com>
2022-05-12 16:06:25 +0200

    fix json jackson

--- src/main/java/com/example/searchservice/Controller/UserController.java
+++ src/main/java/com/example/searchservice/Controller/UserController.java
@@ -1,10 +1,13 @@
-package com.example.searchservice.Controller;
+package com.example.searchservice.controller;
 
 import com.example.searchservice.document.User;
+import com.example.searchservice.search.SearchRequestDTO;
 import com.example.searchservice.service.UserService;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
@@ -25,6 +28,11 @@ public class UserController {
     @GetMapping("/{id}")
     public User findById(@PathVariable final Long id){
         return userService.findById(id);
+    }
+
+    @PostMapping("/search")
+    public List<User> search(@RequestBody final SearchRequestDTO dto) {
+        return userService.search(dto);
     }
 
 
--- src/main/java/com/example/searchservice/service/UserService.java
+++ src/main/java/com/example/searchservice/service/UserService.java
@@ -1,20 +1,67 @@
 package com.example.searchservice.service;
 
 import com.example.searchservice.document.User;
+import com.example.searchservice.helper.Indices;
 import com.example.searchservice.repository.UserRepository;
+import com.example.searchservice.search.SearchRequestDTO;
+import com.example.searchservice.search.util.SearchUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.SearchHit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+    private final UserRepository userRepository;
+    private final RestHighLevelClient client;
+
+    @Autowired
+    public UserService(UserRepository userRepository, RestHighLevelClient client) {
         this.userRepository = userRepository;
+        this.client = client;
+    }
+
+    public List<User> search(final SearchRequestDTO dto) {
+        final SearchRequest request = SearchUtil.buildSearchRequest(
+                Indices.USER_INDEX,
+                dto
+        );
+
+        if (request == null) {
+            LOG.error("Failed to build search request");
+            return Collections.emptyList();
+        }
+
+        try {
+            final SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+
+            final SearchHit[] searchHits = response.getHits().getHits();
+            final List<User> users = new ArrayList<>(searchHits.length);
+            for (SearchHit hit : searchHits) {
+                users.add(
+                        MAPPER.readValue(hit.getSourceAsString(), User.class)
+                );
+            }
+
+            return users;
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return Collections.emptyList();
+        }
     }
 
     public void save(final User user){userRepository.save(user);
