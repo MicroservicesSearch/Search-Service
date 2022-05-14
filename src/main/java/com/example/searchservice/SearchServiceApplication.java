e7144e14eda4b68b8115be5a474bacc842df75fa
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-05-14 11:04:37 +0200

    Setup for rabbitmq in SearchServiceApplication

--- src/main/java/com/example/searchservice/SearchServiceApplication.java
+++ src/main/java/com/example/searchservice/SearchServiceApplication.java
@@ -1,13 +1,45 @@
 package com.example.searchservice;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
+@EnableScheduling
 public class SearchServiceApplication {
+
+    public static final String EXCHANGE_NAME = "exchangeName";
+    public static final String QUEUE_NAME = "queueName";
+    public static final String ROUTING_KEY = "routingKey";
 
     public static void main(String[] args) {
         SpringApplication.run(SearchServiceApplication.class, args);
+    }
+
+    @Bean
+    public TopicExchange appExchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue appQueueSpecific() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding declareBindingSpecific() {
+        return BindingBuilder.bind(appQueueSpecific()).to(appExchange()).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
