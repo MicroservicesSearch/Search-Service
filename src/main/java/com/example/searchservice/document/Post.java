4a2d4de87cfd9ddb626c37d8caa6d4694b60880d
Lukas <lukas.ikvm1@gmail.com>
2022-05-14 12:44:44 +0200

    Add getId

--- src/main/java/com/example/searchservice/document/Post.java
+++ src/main/java/com/example/searchservice/document/Post.java
@@ -42,6 +42,10 @@ public class Post {
         this.created = created;
     }
 
+    public String getId() {
+        return id;
+    }
+
     public void setId(String id) {
         this.id = id;
     }
