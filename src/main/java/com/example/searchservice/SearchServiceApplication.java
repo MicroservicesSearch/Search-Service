c5398435a8e6ba1df50ec1760272d39fde88c149
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-05-14 11:39:10 +0200

    Modified SearchServiceApplication

--- src/main/java/com/example/searchservice/SearchServiceApplication.java
+++ src/main/java/com/example/searchservice/SearchServiceApplication.java
@@ -15,27 +15,43 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class SearchServiceApplication {
 
-    public static final String EXCHANGE_NAME = "exchangeName";
-    public static final String QUEUE_NAME = "queueName";
-    public static final String ROUTING_KEY = "routingKey";
+    public static final String EXCHANGE_POST = "exchangePost";
+    public static final String EXCHANGE_USER = "exchangeUser";
+    public static final String QUEUE_POST = "queuePost";
+    public static final String QUEUE_USER= "queueUser";
+    public static final String ROUTING_KEY_POST = "routingKeyPost";
+    public static final String ROUTING_KEY_USER = "routingKeyUser";
 
     public static void main(String[] args) {
         SpringApplication.run(SearchServiceApplication.class, args);
     }
 
     @Bean
-    public TopicExchange appExchange() {
-        return new TopicExchange(EXCHANGE_NAME);
+    public TopicExchange PostExchange() {
+        return new TopicExchange(EXCHANGE_POST);
+    }
+    @Bean
+    public TopicExchange UserExchange() {
+        return new TopicExchange(EXCHANGE_USER);
     }
 
     @Bean
-    public Queue appQueueSpecific() {
-        return new Queue(QUEUE_NAME);
+    public Queue PostQueue() {
+        return new Queue(QUEUE_POST);
+    }
+    @Bean
+    public Queue UserQueue() {
+        return new Queue(QUEUE_USER);
     }
 
     @Bean
-    public Binding declareBindingSpecific() {
-        return BindingBuilder.bind(appQueueSpecific()).to(appExchange()).with(ROUTING_KEY);
+    public Binding declareBindingPost() {
+        return BindingBuilder.bind(PostQueue()).to(PostExchange()).with(ROUTING_KEY_POST);
+    }
+
+    @Bean
+    public Binding declareBindingUser() {
+        return BindingBuilder.bind(UserQueue()).to(UserExchange()).with(ROUTING_KEY_USER);
     }
 
     @Bean
